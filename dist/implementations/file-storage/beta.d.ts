431573c364eff680d6e91df1f6b425920c7cebfd
Antony Rizzitelli <upd4ting@gmail.com>
2025-12-02 15:12:51 +0100

    chore: remove dist

--- dist/implementations/file-storage/beta.d.ts
+++
@@ -1,8 +0,0 @@
-import { UploadRequest, UploadConstraints, PresignedUploadResponse, PresignedReadResponse, FileMetadata } from '@ajs.local/file-storage/beta';
-export declare namespace internal {
-    const createUploadUrl: (request: UploadRequest, constraints?: UploadConstraints, storage?: string) => Promise<PresignedUploadResponse>;
-    const createReadUrl: (resourceKey: string, expiresIn?: number, storage?: string) => Promise<PresignedReadResponse>;
-    const deleteFile: (resourceKey: string, storage?: string) => Promise<void>;
-    const fileExists: (resourceKey: string, storage?: string) => Promise<boolean>;
-    const getFileMetadata: (resourceKey: string, storage?: string) => Promise<FileMetadata>;
-}
