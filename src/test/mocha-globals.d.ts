48f52575361cdd74ed0838e18cec218159ad4e67
Antony Rizzitelli <upd4ting@gmail.com>
2026-02-14 19:02:28 +0000

    fix(test): add missing mocha afterEach global declaration

--- src/test/mocha-globals.d.ts
+++ src/test/mocha-globals.d.ts
@@ -5,3 +5,4 @@ declare function it(name: string, callback: TestCallback): void;
 declare function before(callback: TestCallback): void;
 declare function after(callback: TestCallback): void;
 declare function beforeEach(callback: TestCallback): void;
+declare function afterEach(callback: TestCallback): void;
