a12beeff4c784f8d0d1a289d4748b66d17720092
Antony Rizzitelli <upd4ting@gmail.com>
2026-02-15 16:12:09 +0100

    chore(deps): replace custom mocha globals with @types packages
    Remove hand-written mocha-globals.d.ts in favor of @types/mocha and
    @types/chai packages. Add chai dependency and configure tsc build
    step in test config.

--- src/test/mocha-globals.d.ts
+++
@@ -1,8 +0,0 @@
-type TestCallback = () => void | Promise<void>;
-
-declare function describe(name: string, callback: TestCallback): void;
-declare function it(name: string, callback: TestCallback): void;
-declare function before(callback: TestCallback): void;
-declare function after(callback: TestCallback): void;
-declare function beforeEach(callback: TestCallback): void;
-declare function afterEach(callback: TestCallback): void;
